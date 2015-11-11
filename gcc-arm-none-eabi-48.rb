9470de7906697ffe1816f9e1f885b3ed554eda60
Fabian-Robert Stöter <faroit@users.noreply.github.com>
2015-11-11 22:24:37 +0100

    update sha

--- gcc-arm-none-eabi-48.rb
+++ gcc-arm-none-eabi-48.rb
@@ -4,7 +4,7 @@ class GccArmNoneEabi48 < Formula
   homepage 'https://launchpad.net/gcc-arm-embdded'
   version '20140805'
   url 'https://launchpad.net/gcc-arm-embedded/4.8/4.8-2014-q3-update/+download/gcc-arm-none-eabi-4_8-2014q3-20140805-mac.tar.bz2'
-  sha1 '1ca44d778fc3b4799d76c98345ed7826'
+  sha1 'd7e8b178fa744b78f6d75a2adfb9b7f554c1a158'
 
   def install 
     ohai 'Copying binaries...'
